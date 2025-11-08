ee765ddf8a6e39afe134e66192b8550a7e1daa03
OBASEKORE, Hammed Olatunde <obasekore.hammed@gmail.com>
2025-11-08 23:49:52 +0900

    Initialize Flask app with YOLO model
    Set up Flask app with YOLO model and CORS.

--- api/index.py
+++ api/index.py
@@ -1,1 +1,16 @@
 
+from flask import Flask, request, jsonify, render_template, url_for
+import requests
+import json
+from flask_cors import CORS
+from ultralytics import YOLO
+
+app = Flask(__name__, static_folder='statics')#, template_folder='/templates')
+model = YOLO("yolo11n.pt")
+
+CORS(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  q = ''
+  pass
