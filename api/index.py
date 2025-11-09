2718e633144b31a109509c99593be54c739982d0
OBASEKORE, Hammed Olatunde <obasekore.hammed@gmail.com>
2025-11-09 14:50:04 +0900

    Disable YOLO model import
    Comment out YOLO model import and initialization.

--- api/index.py
+++ api/index.py
@@ -3,10 +3,10 @@ from flask import Flask, request, jsonify, render_template, url_for
 import requests
 import json
 from flask_cors import CORS
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
 app = Flask(__name__, static_folder='statics')#, template_folder='/templates')
-model = YOLO("yolo11n.pt")
+# model = YOLO("yolo11n.pt")
 
 CORS(app)
 
